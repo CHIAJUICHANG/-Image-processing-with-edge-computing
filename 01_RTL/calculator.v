a6562d06203a43ce86dec31ac3d8fef98293996f
Lab228 <ray12099@gmail.com>
2024-05-16 21:49:22 +0800

    20240516

---
+++ 01_RTL/calculator.v
@@ -0,0 +1,115 @@
+module cal (
+    input  [  8-1:0] cal_x,
+    input  [160-1:0] cal_y,
+    input  [320-1:0] cal_r,
+    output [ 10-1:0] cal_result
+);
+
+wire                 x4_i,  x4_r,  x3_i,  x3_r,  x2_i,  x2_r,  x1_i,  x1_r;
+wire signed [ 8-1:0] y4_i,  y4_r,  y3_i,  y3_r,  y2_i,  y2_r,  y1_i,  y1_r;
+wire signed [ 8-1:0] r44_r, r34_i, r34_r, r24_i, r24_r, r14_i, r14_r, r33_r, r23_i, r23_r, r13_i, r13_r, r22_r, r12_i, r12_r, r11_r;
+
+wire signed [13-1:0] y4_tmp_i, y4_tmp_r, y3_tmp_i, y3_tmp_r, y2_tmp_i, y2_tmp_r, y1_tmp_i, y1_tmp_r;
+wire signed [ 8-1:0] r44s4r_r, r44s4i_i, r33s3r_r, r33s3i_i, r34s4r_r, r34s4i_r, r34s4r_i, r34s4i_i, r22s2r_r, r22s2i_i, r23s3r_r, r23s3i_r, r23s3r_i, r23s3i_i, r24s4r_r, r24s4i_r, 
+                     r24s4r_i, r24s4i_i, r11s1r_r, r11s1i_i, r12s2r_r, r12s2i_r, r12s2r_i, r12s2i_i, r13s3r_r, r13s3i_r, r13s3r_i, r13s3i_i, r14s4r_r, r14s4i_r, r14s4r_i, r14s4i_i;
+
+wire signed [ 8-1:0] y1_ext_i, y1_ext_r, y2_ext_i, y2_ext_r, y3_ext_i, y3_ext_r, y4_ext_i, y4_ext_r;
+
+wire signed [ 8-1:0] entry4_r, entry4_i, entry3_r, entry3_i, entry2_r, entry2_i, entry1_r, entry1_i;
+wire signed [ 9-1:0] square4_r, square4_i, square3_r, square3_i, square2_r, square2_i, square1_r, square1_i;
+
+assign {x4_i, x4_r, x3_i, x3_r, x2_i, x2_r, x1_i, x1_r} = {cal_x};
+assign {y4_i, y4_r, y3_i, y3_r, y2_i, y2_r, y1_i, y1_r} = {cal_y[159:152], cal_y[139:132], cal_y[119:112], cal_y[  99:92], cal_y[  79:72], cal_y[  59:52], cal_y[  39:32], cal_y[  19:12]};
+
+assign r44_r = cal_r[319:312]+1;
+assign r34_i = cal_r[299:292]+1;
+assign r34_r = cal_r[279:272]+1;
+assign r24_i = cal_r[259:252]+1;
+assign r24_r = cal_r[239:232]+1;
+assign r14_i = cal_r[219:212]+1;
+assign r14_r = cal_r[199:192]+1;
+assign r33_r = cal_r[179:172]+1;
+assign r23_i = cal_r[159:152]+1;
+assign r23_r = cal_r[139:132]+1;
+assign r13_i = cal_r[119:112]+1;
+assign r13_r = cal_r[  99:92]+1;
+assign r22_r = cal_r[  79:72]+1;
+assign r12_i = cal_r[  59:52]+1;
+assign r12_r = cal_r[  39:32]+1;
+assign r11_r = cal_r[  19:12]+1;
+
+assign y4_tmp_i = y4_i*5'sb0001_1;
+assign y4_tmp_r = y4_r*5'sb0001_1;
+assign y3_tmp_i = y3_i*5'sb0001_1;
+assign y3_tmp_r = y3_r*5'sb0001_1;
+assign y2_tmp_i = y2_i*5'sb0001_1;
+assign y2_tmp_r = y2_r*5'sb0001_1;
+assign y1_tmp_i = y1_i*5'sb0001_1;
+assign y1_tmp_r = y1_r*5'sb0001_1;
+
+assign r44s4r_r = (x4_r)? -r44_r: r44_r;
+assign r44s4i_i = (x4_i)? -r44_r: r44_r;
+assign r33s3r_r = (x3_r)? -r33_r: r33_r;
+assign r33s3i_i = (x3_i)? -r33_r: r33_r;
+assign r34s4r_r = (x4_r)? -r34_r: r34_r;
+assign r34s4i_r = (x4_i)? -r34_i: r34_i;
+assign r34s4r_i = (x4_r)? -r34_i: r34_i;
+assign r34s4i_i = (x4_i)? -r34_r: r34_r;
+assign r22s2r_r = (x2_r)? -r22_r: r22_r;
+assign r22s2i_i = (x2_i)? -r22_r: r22_r;
+assign r23s3r_r = (x3_r)? -r23_r: r23_r;
+assign r23s3i_r = (x3_i)? -r23_i: r23_i;
+assign r23s3r_i = (x3_r)? -r23_i: r23_i;
+assign r23s3i_i = (x3_i)? -r23_r: r23_r;
+assign r24s4r_r = (x4_r)? -r24_r: r24_r;
+assign r24s4i_r = (x4_i)? -r24_i: r24_i;
+assign r24s4r_i = (x4_r)? -r24_i: r24_i;
+assign r24s4i_i = (x4_i)? -r24_r: r24_r;
+assign r11s1r_r = (x1_r)? -r11_r: r11_r;
+assign r11s1i_i = (x1_i)? -r11_r: r11_r;
+assign r12s2r_r = (x2_r)? -r12_r: r12_r;
+assign r12s2i_r = (x2_i)? -r12_i: r12_i;
+assign r12s2r_i = (x2_r)? -r12_i: r12_i;
+assign r12s2i_i = (x2_i)? -r12_r: r12_r;
+assign r13s3r_r = (x3_r)? -r13_r: r13_r;
+assign r13s3i_r = (x3_i)? -r13_i: r13_i;
+assign r13s3r_i = (x3_r)? -r13_i: r13_i;
+assign r13s3i_i = (x3_i)? -r13_r: r13_r;
+assign r14s4r_r = (x4_r)? -r14_r: r14_r;
+assign r14s4i_r = (x4_i)? -r14_i: r14_i;
+assign r14s4r_i = (x4_r)? -r14_i: r14_i;
+assign r14s4i_i = (x4_i)? -r14_r: r14_r;
+
+assign y4_ext_i = y4_tmp_i[1+:8];
+assign y4_ext_r = y4_tmp_r[1+:8];
+assign y3_ext_i = y3_tmp_i[1+:8];
+assign y3_ext_r = y3_tmp_r[1+:8];
+assign y2_ext_i = y2_tmp_i[1+:8];
+assign y2_ext_r = y2_tmp_r[1+:8];
+assign y1_ext_i = y1_tmp_i[1+:8];
+assign y1_ext_r = y1_tmp_r[1+:8];
+
+assign entry4_r =   y4_ext_r - r44s4r_r;
+assign entry4_i =   y4_ext_i - r44s4i_i;
+assign entry3_r =  (y3_ext_r - r33s3r_r) + (-r34s4r_r + r34s4i_r);
+assign entry3_i =  (y3_ext_i - r33s3i_i) + (-r34s4r_i - r34s4i_i);
+assign entry2_r = ((y2_ext_r - r22s2r_r) + (-r23s3r_r + r23s3i_r)) +  (-r24s4r_r + r24s4i_r);
+assign entry2_i = ((y2_ext_i - r22s2i_i) + (-r23s3r_i - r23s3i_i)) +  (-r24s4r_i - r24s4i_i);
+assign entry1_r = ((y1_ext_r - r11s1r_r) + (-r12s2r_r + r12s2i_r)) + ((-r13s3r_r + r13s3i_r) + (-r14s4r_r + r14s4i_r));
+assign entry1_i = ((y1_ext_i - r11s1i_i) + (-r12s2r_i - r12s2i_i)) + ((-r13s3r_i - r13s3i_i) + (-r14s4r_i - r14s4i_i));
+
+assign square4_r = (entry4_r[7])? (~entry4_r)+1: entry4_r;
+assign square4_i = (entry4_i[7])? (~entry4_i)+1: entry4_i;
+assign square3_r = (entry3_r[7])? (~entry3_r)+1: entry3_r;
+assign square3_i = (entry3_i[7])? (~entry3_i)+1: entry3_i;
+assign square2_r = (entry2_r[7])? (~entry2_r)+1: entry2_r;
+assign square2_i = (entry2_i[7])? (~entry2_i)+1: entry2_i;
+assign square1_r = (entry1_r[7])? (~entry1_r)+1: entry1_r;
+assign square1_i = (entry1_i[7])? (~entry1_i)+1: entry1_i;
+
+assign cal_result = ((square4_r + square4_i) + (square3_r + square3_i)) + ((square2_r + square2_i) + (square1_r + square1_i));
+
+endmodule
+
+
+
---
+++ 01_RTL/ml_demodulator.v
@@ -0,0 +1,295 @@
+module ml_demodulator(
+    i_clk,
+    i_reset,
+    i_trig,
+    i_y_hat,
+    i_r,
+    i_rd_rdy,
+    o_rd_vld,
+    o_llr,
+    o_hard_bit
+);
+
+input            i_clk;
+input            i_reset;
+input            i_trig;
+input  [160-1:0] i_y_hat;
+input  [320-1:0] i_r;
+input            i_rd_rdy;
+output           o_rd_vld;
+output [  8-1:0] o_llr;
+output           o_hard_bit;
+
+localparam INF = 10'b01_1111_1111;
+// ---------------------------------------------------------------------------
+// Declaration
+// ---------------------------------------------------------------------------
+reg            i_trig_d;
+reg  [160-1:0] i_y_hat_d;
+reg  [320-1:0] i_r_d;
+
+reg  [  8-1:0] w_ptr, r_ptr;
+reg  [256-1:0] buffer;
+
+wire [  8-1:0] pattern0, pattern1, pattern2, pattern3;
+wire [ 10-1:0] cal_result0, cal_result1, cal_result2, cal_result3;
+
+wire           cal_done;
+reg            busy;
+reg            cnt1000;
+reg  [  6-1:0] cycle_cnt;
+
+wire           gclk_in, gclk_out;
+reg            in_en, out_en;
+
+wire signed [ 10-1:0] min_x42, min_x41, min_x32, min_x31, min_x22, min_x21;
+wire signed [ 10-1:0] min_x42_n, min_x41_n, min_x32_n, min_x31_n, min_x22_n, min_x21_n;
+wire signed [ 10-1:0] local_min, cal_min01, cal_min23, cal_min13, cal_min02;
+
+wire signed [ 10-1:0] min_x42_1,   min_x41_1,   min_x32_1,   min_x31_1,   min_x22_1,   min_x21_1,   min_x12_1,   min_x11_1;
+wire signed [ 10-1:0] min_x42_0,   min_x41_0,   min_x32_0,   min_x31_0,   min_x22_0,   min_x21_0,   min_x12_0,   min_x11_0;
+reg  signed [ 10-1:0] min_x42_1_n, min_x41_1_n, min_x32_1_n, min_x31_1_n, min_x22_1_n, min_x21_1_n, min_x12_1_n, min_x11_1_n;
+reg  signed [ 10-1:0] min_x42_0_n, min_x41_0_n, min_x32_0_n, min_x31_0_n, min_x22_0_n, min_x21_0_n, min_x12_0_n, min_x11_0_n;
+
+wire signed [ 10-1:0] llr_x11, llr_x12, llr_x21, llr_x22, llr_x31, llr_x32, llr_x41, llr_x42;
+wire                  hb_x11, hb_x12, hb_x21, hb_x22, hb_x31, hb_x32, hb_x41, hb_x42;
+
+// ---------------------------------------------------------------------------
+// Instantiation
+// ---------------------------------------------------------------------------
+cal CAL_U0 (
+    .cal_x      (pattern0),
+    .cal_y      (i_y_hat_d),
+    .cal_r      (i_r_d),
+    .cal_result (cal_result0)
+); 
+cal CAL_U1 (
+    .cal_x      (pattern1),
+    .cal_y      (i_y_hat_d),
+    .cal_r      (i_r_d),
+    .cal_result (cal_result1)
+); 
+cal CAL_U2 (
+    .cal_x      (pattern2),
+    .cal_y      (i_y_hat_d),
+    .cal_r      (i_r_d),
+    .cal_result (cal_result2)
+); 
+cal CAL_U3 (
+    .cal_x      (pattern3),
+    .cal_y      (i_y_hat_d),
+    .cal_r      (i_r_d),
+    .cal_result (cal_result3)
+);
+
+// ---------------------------------------------------------------------------
+// Controller
+// ---------------------------------------------------------------------------
+assign cal_done = (busy && (cycle_cnt == 63))? 1: 0;
+
+always@(posedge i_clk or posedge i_reset) begin
+    if(i_reset)                 cycle_cnt <= 0;
+    else if(busy)               cycle_cnt <= cycle_cnt+1;
+end
+
+always@(posedge i_clk or posedge i_reset) begin
+    if(i_reset)                             busy <= 0;
+    else if(i_trig)                         busy <= 1;
+    else if(cycle_cnt == 63)                busy <= 0;
+end
+
+
+always@(posedge i_clk or posedge i_reset) begin
+    if(i_reset)                             cnt1000 <= 0;
+    else if ((!i_trig) && cal_done && busy) cnt1000 <= 0;
+end
+
+// ---------------------------------------------------------------------------
+// Gating Clock
+// ---------------------------------------------------------------------------
+assign gclk_in = i_clk & in_en;
+assign gclk_out = i_clk & out_en;
+
+always@(negedge i_clk or posedge i_reset) begin
+   if(i_reset)          in_en <= 0;
+    else                in_en <= i_trig; 
+end
+
+always@(negedge i_clk or posedge i_reset) begin
+   if(i_reset)   out_en <= 0;
+    else         out_en <= cal_done && (!cnt1000); 
+end
+
+// ---------------------------------------------------------------------------
+// Input DFF
+// ---------------------------------------------------------------------------
+always@(posedge i_clk or posedge i_reset) begin
+    if(i_reset)         i_trig_d <= 0;
+    else                i_trig_d <= i_trig;
+end
+always@(posedge gclk_in or posedge i_reset) begin
+    if(i_reset)         i_y_hat_d <= 0;
+    else                i_y_hat_d <= i_y_hat;
+end
+always@(posedge gclk_in or posedge i_reset) begin
+    if(i_reset)         i_r_d <= 0;
+    else                i_r_d <= i_r;
+end
+
+// ---------------------------------------------------------------------------
+// Output Buffer
+// ---------------------------------------------------------------------------
+assign o_rd_vld   = (w_ptr != r_ptr)? 1: 0;
+assign o_llr      = (o_rd_vld)? {buffer[r_ptr], 7'b0000001}: 0;
+assign o_hard_bit = (o_rd_vld)? buffer[r_ptr]: 0;
+
+always@(posedge gclk_out or posedge i_reset) begin
+    if(i_reset)                       buffer <= 0;
+    else       buffer[w_ptr+: 8] <= {hb_x42, hb_x41, hb_x32, hb_x31, hb_x22, hb_x21, hb_x12, hb_x11};
+end
+
+always@(posedge i_clk or posedge i_reset) begin
+    if(i_reset)                       w_ptr <= 0;
+    else if(cal_done && (!cnt1000))   w_ptr <= w_ptr + 8;
+end
+
+always@(posedge i_clk or posedge i_reset) begin
+    if(i_reset)                       r_ptr <= 0;
+    else if(o_rd_vld && i_rd_rdy)     r_ptr <= r_ptr + 1;
+end
+
+// ---------------------------------------------------------------------------
+// QPSK Constellation Generator
+// ---------------------------------------------------------------------------
+assign pattern0 = {cycle_cnt, 2'b00};
+assign pattern1 = {cycle_cnt, 2'b01};
+assign pattern2 = {cycle_cnt, 2'b10};
+assign pattern3 = {cycle_cnt, 2'b11};
+
+// ---------------------------------------------------------------------------
+// Comparator
+// ---------------------------------------------------------------------------
+assign cal_min01 = (cal_result0 < cal_result1)? cal_result0: cal_result1;
+assign cal_min23 = (cal_result2 < cal_result3)? cal_result2: cal_result3;
+assign cal_min13 = (cal_result1 < cal_result3)? cal_result1: cal_result3;
+assign cal_min02 = (cal_result0 < cal_result2)? cal_result0: cal_result2;
+assign local_min = (  cal_min01 < cal_min23  )? cal_min01  : cal_min23;
+// x42
+assign min_x42_n = (           cycle_cnt[5])? min_x42_1_n: min_x42_0_n;
+assign min_x42   = (  min_x42_n < local_min)? min_x42_n  : local_min;
+// x41
+assign min_x41_n = (           cycle_cnt[4])? min_x41_1_n: min_x41_0_n;
+assign min_x41   = (  min_x41_n < local_min)? min_x41_n  : local_min;
+// x32
+assign min_x32_n = (           cycle_cnt[3])? min_x32_1_n: min_x32_0_n;
+assign min_x32   = (  min_x32_n < local_min)? min_x32_n  : local_min;
+// x31
+assign min_x31_n = (           cycle_cnt[2])? min_x31_1_n: min_x31_0_n;
+assign min_x31   = (  min_x31_n < local_min)? min_x31_n  : local_min;
+// x22
+assign min_x22_n = (           cycle_cnt[1])? min_x22_1_n: min_x22_0_n;
+assign min_x22   = (  min_x22_n < local_min)? min_x22_n  : local_min;
+// x21
+assign min_x21_n = (           cycle_cnt[0])? min_x21_1_n: min_x21_0_n;
+assign min_x21   = (  min_x21_n < local_min)? min_x21_n  : local_min;
+// x12
+assign min_x12_1 = (min_x12_1_n < cal_min23)? min_x12_1_n: cal_min23;
+assign min_x12_0 = (min_x12_0_n < cal_min01)? min_x12_0_n: cal_min01;
+// x11
+assign min_x11_1 = (min_x11_1_n < cal_min13)? min_x11_1_n: cal_min13;
+assign min_x11_0 = (min_x11_0_n < cal_min02)? min_x11_0_n: cal_min02;
+
+always@(posedge i_clk or posedge i_reset) begin
+    if(i_reset) begin
+        min_x42_1_n <= INF;
+        min_x42_0_n <= INF;
+        min_x41_1_n <= INF;
+        min_x41_0_n <= INF;
+        min_x32_1_n <= INF;
+        min_x32_0_n <= INF;
+        min_x31_1_n <= INF;
+        min_x31_0_n <= INF;
+        min_x22_1_n <= INF;
+        min_x22_0_n <= INF;
+        min_x21_1_n <= INF;
+        min_x21_0_n <= INF;
+        min_x12_1_n <= INF;
+        min_x12_0_n <= INF;
+        min_x11_1_n <= INF;
+        min_x11_0_n <= INF;
+    end else if(cal_done) begin
+        min_x42_1_n <= INF;
+        min_x42_0_n <= INF;
+        min_x41_1_n <= INF;
+        min_x41_0_n <= INF;
+        min_x32_1_n <= INF;
+        min_x32_0_n <= INF;
+        min_x31_1_n <= INF;
+        min_x31_0_n <= INF;
+        min_x22_1_n <= INF;
+        min_x22_0_n <= INF;
+        min_x21_1_n <= INF;
+        min_x21_0_n <= INF;
+        min_x12_1_n <= INF;
+        min_x12_0_n <= INF;
+        min_x11_1_n <= INF;
+        min_x11_0_n <= INF;
+    end else if(i_trig_d || busy) begin
+        min_x42_1_n <= ( cycle_cnt[5])? min_x42: min_x42_1_n;
+        min_x42_0_n <= (~cycle_cnt[5])? min_x42: min_x42_0_n;
+        min_x41_1_n <= ( cycle_cnt[4])? min_x41: min_x41_1_n;
+        min_x41_0_n <= (~cycle_cnt[4])? min_x41: min_x41_0_n;
+        min_x32_1_n <= ( cycle_cnt[3])? min_x32: min_x32_1_n;
+        min_x32_0_n <= (~cycle_cnt[3])? min_x32: min_x32_0_n;
+        min_x31_1_n <= ( cycle_cnt[2])? min_x31: min_x31_1_n;
+        min_x31_0_n <= (~cycle_cnt[2])? min_x31: min_x31_0_n;
+        min_x22_1_n <= ( cycle_cnt[1])? min_x22: min_x22_1_n;
+        min_x22_0_n <= (~cycle_cnt[1])? min_x22: min_x22_0_n;
+        min_x21_1_n <= ( cycle_cnt[0])? min_x21: min_x21_1_n;
+        min_x21_0_n <= (~cycle_cnt[0])? min_x21: min_x21_0_n;
+        min_x12_1_n <= min_x12_1;
+        min_x12_0_n <= min_x12_0;
+        min_x11_1_n <= min_x11_1;
+        min_x11_0_n <= min_x11_0;
+    end
+end
+
+// ---------------------------------------------------------------------------
+// Log Likelihood Ratio & Hard Bit
+// ---------------------------------------------------------------------------
+assign min_x42_1 = ( cycle_cnt[5])? min_x42: min_x42_1_n;
+assign min_x42_0 = (~cycle_cnt[5])? min_x42: min_x42_0_n;
+assign min_x41_1 = ( cycle_cnt[4])? min_x41: min_x41_1_n;
+assign min_x41_0 = (~cycle_cnt[4])? min_x41: min_x41_0_n;
+assign min_x32_1 = ( cycle_cnt[3])? min_x32: min_x32_1_n;
+assign min_x32_0 = (~cycle_cnt[3])? min_x32: min_x32_0_n;
+assign min_x31_1 = ( cycle_cnt[2])? min_x31: min_x31_1_n;
+assign min_x31_0 = (~cycle_cnt[2])? min_x31: min_x31_0_n;
+assign min_x22_1 = ( cycle_cnt[1])? min_x22: min_x22_1_n;
+assign min_x22_0 = (~cycle_cnt[1])? min_x22: min_x22_0_n;
+assign min_x21_1 = ( cycle_cnt[0])? min_x21: min_x21_1_n;
+assign min_x21_0 = (~cycle_cnt[0])? min_x21: min_x21_0_n;
+
+assign llr_x42 = (cal_done)? (min_x42_1 - min_x42_0): 0;
+assign llr_x41 = (cal_done)? (min_x41_1 - min_x41_0): 0;
+assign llr_x32 = (cal_done)? (min_x32_1 - min_x32_0): 0;
+assign llr_x31 = (cal_done)? (min_x31_1 - min_x31_0): 0;
+assign llr_x22 = (cal_done)? (min_x22_1 - min_x22_0): 0;
+assign llr_x21 = (cal_done)? (min_x21_1 - min_x21_0): 0;
+assign llr_x12 = (cal_done)? (min_x12_1 - min_x12_0): 0;
+assign llr_x11 = (cal_done)? (min_x11_1 - min_x11_0): 0;
+
+assign hb_x42 = llr_x42[9];
+assign hb_x41 = llr_x41[9];
+assign hb_x32 = llr_x32[9];
+assign hb_x31 = llr_x31[9];
+assign hb_x22 = llr_x22[9];
+assign hb_x21 = llr_x21[9];
+assign hb_x12 = llr_x12[9];
+assign hb_x11 = llr_x11[9];
+
+endmodule
+
+
+
+
